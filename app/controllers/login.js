a9ab1b9c1619100299ce49400182a82a1f19d5ed
nicmitchell <nic@nicmitchell.com>
2014-09-26 12:30:17 -0700

    login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -3,9 +3,9 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   actions: {
     authenticate: function() {
-      var user = this.get('username');
+      var username = this.get('username');
       var password = this.get('password');
-      var activeUser = this.store.find(user);
+      var activeUser = this.store.find(username);
       if (activeUser.password === password){
         activeUser.isAuthenticated = true;
       this.transitionTo('dashboard');
--- app/controllers/register/index.js
+++ app/controllers/register/index.js
@@ -2,13 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   actions: {
-    registerUser: function(){
+    register: function(){
       this.store.createRecord('user', {
         id: this.get('user'),
         name: this.get('name'),
         email: this.get('email'),
         password: this.get('password'),
-
       });
       this.transitionTo('dashboard');
     }
